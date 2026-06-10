9e171e857448fb27c462e98036081fd561f16d56
zeusflag699-boop <zeusflag699@gmail.com>
2026-06-09 20:42:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ if (botonModo) {
         document.body.classList.toggle("modo-oscuro");
     });
 }
+
+// Botón descargar
+const botonDescargar = document.querySelector("#descargarBtn");
+const mensaje = document.querySelector("#mensaje");
+
+if (botonDescargar) {
+    botonDescargar.addEventListener("click", () => {
+        mensaje.textContent =
+            "¡Gracias por tu interés en WolvesX!";
+    });
+}
